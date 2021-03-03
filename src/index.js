128b36626779f00ad188aa793588ff3715f01f36
Valeriia <valeriastanis@gmail.com>
2021-03-03 23:11:22 +0100

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function toReadable (number) {
   
+    var ones = new Array('', ' one', ' two', ' three', ' four', ' five', ' six', ' seven', ' eight', ' nine', ' ten', ' eleven', ' twelve', ' thirteen', ' fourteen', ' fifteen', ' sixteen', ' seventeen', ' eighteen', ' nineteen');
+    var tens = new Array('', '', ' twenty', ' thirty', ' forty', ' fifty', ' sixty', ' seventy', ' eighty', ' ninety');
+
+  if(number == 0) {
+      return 'zero'
+  }
+  
+  if (number<20) {
+    return (ones[number]).trim()
+  } 
+  
+  if (number<100) {
+        return (tens[Math.floor(number/10)] + ones[number%10]).trim()
+  }
+  
+  if(number<1000) {
+    hundreds = ones[Math.floor(number/100)] + ' hundred '
+    tens_number = number%100==0 ? '' : toReadable(number%100)
+    return (hundreds + tens_number).trim()
+  } 
 }
